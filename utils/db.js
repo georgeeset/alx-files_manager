3447ffb2362b032f0eaa2a0d3b65fca358797f4c
George <georgeperfect4u@gmail.com>
2023-12-02 03:56:54 +0100

    mongodb client fix

--- utils/db.js
+++ utils/db.js
@@ -1,43 +1,36 @@
 import { MongoClient } from 'mongodb';
 // const { MongoClient } = require('mongodb');
 
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
 class DBClient {
   constructor() {
-    this.host = process.env.BD_HOST || 'localhost';
-    this.port = process.env.PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.client = new MongoClient(`mongodb://${this.host}:${this.port}/${this.database}`, { useUnifiedTopology: true });
-
-    this.client.connect()
-      .then(() => {
-        console.log('Mongo bd connected');
-      }).catch((error) => {
-        console.log(error);
-      });
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(DB_DATABASE);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
   }
 
-  isAlive() {
-    return this.client.isConnected();
-  }
+  isAlive() { return !!this.db; }
 
-  async nbUsers() {
-		if (this.isAlive()){
-    	const userColletion = this.client.db().collection('users');
-    	const userNumber = await userColletion.countDocuments();
-    	return userNumber;
-		}
-  }
+  async nbUsers() { return this.users.countDocuments(); }
 
-  async nbFiles() {
-    
-		if (this.isAlive()){
-			// access the collectiom
+  async nbFiles() { return this.files.countDocuments(); }
 
-    	const filesColletion = this.client.db().collection('files');
-    	const fileNumber = await filesColletion.countDocuments();
-    	return fileNumber;
-		}
+  async getUser(query) {
+    const user = await this.db.collection('users').findOne(query);
+    return user;
   }
 }
-const dbclient = new DBClient();
-export default dbclient;
+
+const dbClient = new DBClient();
+module.exports = dbClient;
