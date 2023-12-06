366373e5b8e9d7f5207cedc5e17ddcc535d58c4d
Benonking <benonkighoma@gmail.com>
2023-12-06 22:31:11 +0300

    check db staus with API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,15 +1,14 @@
-import redisClient from "../utils/redis";
-import dbclient from "../utils/db";
+import redisClient from '../utils/redis';
+import dbclient from '../utils/db';
 
 const getStatus = async (req, res) => {
-	const redisStatus = redisClient.isAlive();
-	const dbStatus = dbclient.isAlive();
-	return res.status(200).json({redis: redisStatus, db: dbStatus});
+  const redisStatus = await redisClient.isAlive();
+  const dbStatus = await dbclient.isAlive();
+  return res.status(200).json({ redis: redisStatus, db: dbStatus });
 };
 const getStats = async (req, res) => {
-	const userNumber = dbclient.nbUsers();
-	const fileNumber = dbclient.nbFiles();
-	return res.status(200). json({users: userNumber, files: fileNumber});
+  const userNumber = await dbclient.nbUsers();
+  const fileNumber = await dbclient.nbFiles();
+  return res.status(200).json({ users: userNumber, files: fileNumber });
 };
 module.exports = { getStatus, getStats };
-
