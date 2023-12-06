d63976522276f12d4d709ca9c5bf11a5e4b40305
Benonking <benonkighoma@gmail.com>
2023-12-06 21:19:42 +0300

    add redis client
    handle eslint

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
-const UserController = require('../controllers/UserController');
+const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-
+const FilesController = require('../collection/FilesController');
 const router = express.Router();
 router.get('/status', AppController.getStats);
 router.get('/stats', AppController.getStats);
-router.post('/users', UserController.postNew);
+router.post('/users', UsersController.postNew);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/connect', AuthController.getConnect);
 router.get('/users/me', AuthController.getMe);
 
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
